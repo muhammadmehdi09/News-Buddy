e3d4046d8425e739bccd9b38e67b17c71c7e5125
muhammadmehdi09 <120133578+muhammadmehdi09@users.noreply.github.com>
2024-02-23 15:39:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+let news = document.getElementById('news')
+let search = document.getElementById('search')
+let resultNum = document.getElementById('resultNum')
+let btn = document.getElementById('btn')
+let nextpage = ""
+
+const newsBringer = async () => {
+  var url = 'https://newsdata.io/api/1/news?' +
+    `apikey=pub_382913f3350be2fba3e927c99fdad97348631&` +
+    `q=${search.value}&` +
+    `language=en` 
+
+  const objecturl = await fetch(url)
+  const object = await objecturl.json()
+  nextpage = `page=${object.nextPage}`
+  fetchNews(object)
+}
+
+const increasePage = async () => {
+  var url = 'https://newsdata.io/api/1/news?' +
+    `apikey=pub_382913f3350be2fba3e927c99fdad97348631&` +
+    `q=${search.value}&` +
+    `language=en&` +
+    `${nextpage}`
+
+  const objecturl = await fetch(url)
+  const object = await objecturl.json()
+  nextpage = `page=${object.nextPage}`
+  fetchNews(object)
+}
+
+const fetchNews = async (object) => {
+
+
+  news.innerHTML = ""
+  if (object.totalResults != undefined) {
+    resultNum.innerHTML = `(${object.totalResults}) Results Found for ${search.value}`
+  }
+
+  for (let item of object.results) {
+    let description = item.description
+    let myArray = description.split(" ")
+    let length = myArray.length
+    let num = 2
+    if (item.image_url === null || undefined) {
+      num = "270px"
+    }
+    if (length > 20) {
+      myArray.splice(20, (length - 1));
+    }
+    myArray.push("...")
+    myArray = myArray.join(" ")
+
+    news.innerHTML = news.innerHTML + `
+    <div class="place-items-center min-[2100px]:w-[400px] min-[1800px]:w-[350px] min-[1950px]:w-[375px] w-72 bg-white mx-auto place-self-center p-4 min-h-[620px] md:max-h-[620px]">
+    <img src="${item.image_url}" class="min-[1500px]:max-h-[130px] mx-auto min-[1800px]:max-h-[160px] min-[1950px]:max-h-[200px]" alt="Sorry Image Cannot be loaded">    
+    <h2 class="text-sky-400 text-center text-xl font-semibold mt-[${num}]">${item.title}</h2>
+        <p class="mt-5 mx-8 text-center">${myArray}</p>
+          <a href="${item.link}" class="place-items-center -mr-20" target="_blank"><button
+            class="bg-red-500 px-8 min-[2100px]:ml-[80px] min-[1800px]:ml-[75px] min-[1600px]:ml-[70px] ml-[65px] py-3 rounded-xl mt-10 min-[1800px]:px-12 min-[1800px]:py-5 min-[1800px]:text-lg">Read
+            More</button></a>
+      </div>
+      `
+  }
+  console.log(object);
+}
