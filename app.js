39d9526793de2f36dd36aa21b2af4c8d403a0e71
Coding Verse <120133578+muhammadmehdi09@users.noreply.github.com>
2024-05-04 12:48:17 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 let news = document.getElementById('news')
 let search = document.getElementById('search')
+let miniSearch = document.getElementById('min-search')
 let resultNum = document.getElementById('resultNum')
 let btn = document.getElementById('btn')
 let nextpage = ""
+let searchKey
+
+if(searchKey === undefined || null){
+   searchKey = miniSearch
+}
+else{
+  searchKey = search
+}
 
 const newsBringer = async () => {
   var url = 'https://newsdata.io/api/1/news?' +
     `apikey=pub_382913f3350be2fba3e927c99fdad97348631&` +
-    `q=${search.value}&` +
-    `language=en` 
+    `q=${searchKey}&` +
+    `language=en`
 
   const objecturl = await fetch(url)
   const object = await objecturl.json()
@@ -20,7 +29,7 @@ const newsBringer = async () => {
 const increasePage = async () => {
   var url = 'https://newsdata.io/api/1/news?' +
     `apikey=pub_382913f3350be2fba3e927c99fdad97348631&` +
-    `q=${search.value}&` +
+    `q=${searchKey}&` +
     `language=en&` +
     `${nextpage}`
 
@@ -31,11 +40,9 @@ const increasePage = async () => {
 }
 
 const fetchNews = async (object) => {
-
-
   news.innerHTML = ""
   if (object.totalResults != undefined) {
-    resultNum.innerHTML = `(${object.totalResults}) Results Found for ${search.value}`
+    resultNum.innerHTML = `(${object.totalResults}) Results Found for ${searchKey}`
   }
 
   for (let item of object.results) {
@@ -44,21 +51,24 @@ const fetchNews = async (object) => {
     let length = myArray.length
     let num = 2
     if (item.image_url === null || undefined) {
-      num = "270px"
+      num = "170px"
     }
     if (length > 20) {
       myArray.splice(20, (length - 1));
     }
     myArray.push("...")
     myArray = myArray.join(" ")
-
+    btn.innerHTML =
+      `<button class="bg-red-500 px-8 py-3 rounded-xl -mb-96 place-self-center mt-10" onclick="increasePage()">
+          Next Page >
+    </button>`
     news.innerHTML = news.innerHTML + `
-    <div class="place-items-center min-[2100px]:w-[400px] min-[1800px]:w-[350px] min-[1950px]:w-[375px] w-72 bg-white mx-auto place-self-center p-4 min-h-[620px] md:max-h-[620px]">
-    <img src="${item.image_url}" class="min-[1500px]:max-h-[130px] mx-auto min-[1800px]:max-h-[160px] min-[1950px]:max-h-[200px]" alt="Sorry Image Cannot be loaded">    
-    <h2 class="text-sky-400 text-center text-xl font-semibold mt-[${num}]">${item.title}</h2>
+    <div class="place-items-center min-[2100px]:w-[400px] min-[1800px]:w-[350px] min-[1950px]:w-[375px] w-72 bg-white mx-auto place-self-center p-4 min-h-[620px] md:max-h-[620px] relative">
+    <img src="${item.image_url}" class="min-[1500px]:max-h-[130px] mx-auto min-[1800px]:max-h-[160px] min-[1950px]:max-h-[200px] mb-10" alt="Sorry Image Cannot be loaded">    
+    <h2 class="text-sky-400 text-center text-xl font-semibold mt-[${num}] overflow-hidden">${item.title}</h2>
         <p class="mt-5 mx-8 text-center">${myArray}</p>
           <a href="${item.link}" class="place-items-center -mr-20" target="_blank"><button
-            class="bg-red-500 px-8 min-[2100px]:ml-[80px] min-[1800px]:ml-[75px] min-[1600px]:ml-[70px] ml-[65px] py-3 rounded-xl mt-10 min-[1800px]:px-12 min-[1800px]:py-5 min-[1800px]:text-lg">Read
+            class="absolute bg-red-500 px-8 bottom-10 min-[2100px]:ml-[80px] min-[1800px]:ml-[75px] min-[1600px]:ml-[70px] ml-[65px] py-3 rounded-xl mt-10 min-[1800px]:px-12 min-[1800px]:py-5 min-[1800px]:text-lg">Read
             More</button></a>
       </div>
       `
